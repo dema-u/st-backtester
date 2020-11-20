24a021c86a158748398c8574e79c6d69a3c44d61
Dema Ushchapovskyy <dema.ushchapovskyy@gmail.com>
2020-11-20 18:56:52 +0000

    Working version of data download

--- data.py
+++ data.py
@@ -2,12 +2,20 @@ import os
 import configparser
 import pandas as pd
 import fxcmpy
+from tqdm import tqdm
 from datetime import datetime
 from utils import DataManager
 
 
+FREQ_MAP = {'m5': '5min', 'm15': '15min', 'm30': '30min', 'H1': '1H', 'H4': '4H', 'D1': '1D', 'W1': '1W'}
+
+
+def format_ticker_str(ticker: str) -> str:
+    return ticker[:3] + '/' + ticker[3:]
+
+
 def get_ticker_data(con, ticker: str, freq: str, first_date: datetime, last_date: datetime) -> pd.DataFrame:
-    dates = list(pd.date_range(start=first_date, end=last_date, freq=freq)[::10000]) + [last_date]
+    dates = list(pd.date_range(start=first_date, end=last_date, freq=FREQ_MAP[freq])[::10000]) + [last_date]
 
     data = [con.get_candles(instrument=ticker, period=freq, start=dates[ix], stop=dates[ix + 1]) for ix in
             range(len(dates) - 1)]
@@ -34,20 +42,15 @@ if __name__ == '__main__':
 
     data_section = config['DATA']
 
-    all_tickers = data_section['tickers'].split(',')
+    all_tickers = [format_ticker_str(ticker) for ticker in data_section['tickers'].split(',')]
     all_freqs = data_section['frequency'].split(',')
     first_date = datetime.strptime(data_section['first_date'], '%d/%m/%Y')
-    last_date = datetime.strptime(data_section['first_date'], '%d/%m/%Y')
+    last_date = datetime.strptime(data_section['last_date'], '%d/%m/%Y')
 
-    print(all_tickers)
-    print(all_freqs)
-
-    for ticker in all_tickers:
+    for ticker in tqdm(all_tickers):
         for freq in all_freqs:
-            data = api_con.get_candles(ticker, period=freq)
-            print(data)
-            print(freq)
-            data = get_ticker_data(api_con, ticker=ticker, freq=freq.strip(), first_date=first_date, last_date=last_date)
-            DataManager.store_price_data(data, ticker, freq, raw=True)
+            data = get_ticker_data(api_con, ticker=ticker, freq=freq, first_date=first_date, last_date=last_date)
+            ticker_name = ticker.replace('/', '')
+            DataManager.store_price_data(data, ticker_name, freq, raw=True)
 
     print('PROGRAM FINISHED')
--- utils.py
+++ utils.py
@@ -5,50 +5,51 @@ from typing import List
 
 class DataManager:
 
-    DATA_FOLDER = 'data'
+    _DATA_FOLDER = 'data'
 
-    RAW_FOLDER = 'raw'
-    CLEAN_FOLDER = 'processed'
+    _RAW_FOLDER = 'raw'
+    _CLEAN_FOLDER = 'processed'
 
-    PRICES_FOLDER = 'prices'
+    _PRICES_FOLDER = 'prices'
 
     @staticmethod
     def create_paths(base_data_path):
         os.mkdir(base_data_path)
-        os.mkdir(os.path.join(base_data_path, DataManager.RAW_FOLDER))
-        os.mkdir(os.path.join(base_data_path, DataManager.CLEAN_FOLDER))
+        os.mkdir(os.path.join(base_data_path, DataManager._RAW_FOLDER))
+        os.mkdir(os.path.join(base_data_path, DataManager._CLEAN_FOLDER))
 
     @staticmethod
     def store_price_data(data: pd.DataFrame, ticker: str, freq: str, raw: bool) -> None:
-        base_data_path = os.path.abspath(DataManager.DATA_FOLDER)
+        base_data_path = os.path.abspath(DataManager._DATA_FOLDER)
 
         if not os.path.exists(base_data_path):
             DataManager.create_paths(base_data_path)
 
         if raw:
-            data_path = os.path.join(base_data_path, DataManager.RAW_FOLDER)
+            data_path = os.path.join(base_data_path, DataManager._RAW_FOLDER)
         else:
-            data_path = os.path.join(base_data_path, DataManager.CLEAN_FOLDER)
+            data_path = os.path.join(base_data_path, DataManager._CLEAN_FOLDER)
 
-        freq_path = os.path.join(data_path, DataManager.PRICES_FOLDER, freq)
+        freq_path = os.path.join(data_path, DataManager._PRICES_FOLDER, freq)
 
         if not os.path.exists(freq_path):
             os.mkdir(freq_path)
 
-        full_data_path = os.path.join(freq_path, ticker)
+        ticker_file = ticker + '.csv'
+        full_data_path = os.path.join(freq_path, ticker_file)
         data.to_csv(full_data_path)
 
     @staticmethod
     def read_price_data(ticker: str, freq: str, raw: bool) -> pd.DataFrame:
-        base_data_path = os.path.abspath(DataManager.DATA_FOLDER)
+        base_data_path = os.path.abspath(DataManager._DATA_FOLDER)
         ticker_file = ticker + '.csv'
 
         if raw:
-            data_path = os.path.join(base_data_path, DataManager.RAW_FOLDER)
+            data_path = os.path.join(base_data_path, DataManager._RAW_FOLDER)
         else:
-            data_path = os.path.join(base_data_path, DataManager.CLEAN_FOLDER)
+            data_path = os.path.join(base_data_path, DataManager._CLEAN_FOLDER)
 
-        file_path = os.path.join(data_path, DataManager.PRICES_FOLDER, freq, ticker_file)
+        file_path = os.path.join(data_path, DataManager._PRICES_FOLDER, freq, ticker_file)
 
         print(file_path)
 
